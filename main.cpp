2c3fedda32e16aa1fd4283bbd6c0ed82a14f8ed1
Kevin Krammer <kevin.krammer@kdab.com>
2012-10-15 18:58:50 +0200

    Make QML file a commandline option

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include "declarativewidgetdocument.h"
 
+#include <QtCore/QDir>
+#include <QtCore/QFileInfo>
 #include <QtCore/QTimer>
 #include <QtGui/QApplication>
 #include <QtGui/QWidget>
-
+#include <QDebug>
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
+
+  const QStringList arguments = app.arguments();
+  if (arguments.count() < 2) {
+      qFatal("Missing QML file argument. Usage: qmlwidgets <qmlfile>");
+      return -1;
+  }
 
   QTimer timer;
   timer.setInterval(1000);
   timer.start();
 
-  DeclarativeWidgetDocument document(QUrl("test.qml"));
-  //DeclarativeWidgetDocument document(QUrl("animation.qml"));
+  const QFileInfo qmlFile(QDir::current(), arguments[1]);
+  const QUrl documentUrl = QUrl::fromLocalFile(qmlFile.absoluteFilePath());
+
+  DeclarativeWidgetDocument document(documentUrl);
   document.setContextProperty("_timer", &timer);
 
   QWidget *widget = document.create<QWidget>();
