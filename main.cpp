8ec28e1130ff2434951167918c22d4eb94e47191
Tobias Koenig <tobias.koenig@kdab.com>
2012-10-28 12:56:54 +0100

    Use qWarning instead of qFatal to quit app

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 
   const QStringList arguments = app.arguments();
   if (arguments.count() < 2) {
-      qFatal("Missing QML file argument. Usage: qmlwidgets <qmlfile>");
+      qWarning("Missing QML file argument. Usage: qmlwidgets <qmlfile>");
       return -1;
   }
 
