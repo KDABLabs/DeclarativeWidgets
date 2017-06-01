626039f9005fa2801c10713a2072d0db24b4c9df
Nathan Collins <nathan.collins@kdab.com>
2017-06-01 15:04:31 +0100

    Added DeclarativeContextProperty test
    It currently fails.

--- tests/tst_declarativeview.cpp
+++ tests/tst_declarativeview.cpp
@@ -2,6 +2,7 @@
 #include <QtQml>
 #include <QtTest/QtTest>
 
+#include <QQuickItem>
 #include <QQuickWidget>
 
 #include "declarativedeclarativecontext_p.h"
@@ -16,6 +17,7 @@ public:
 private slots:
     void instantiateQQuickWidget();
     void instantiateQQuickWidgetWithContext();
+    void instantiateQQuickWidgetWithContextProperty();
 };
 
 tst_DeclarativeView::tst_DeclarativeView()
@@ -63,6 +65,36 @@ void tst_DeclarativeView::instantiateQQuickWidgetWithContext()
 
     // Verify that the root context hasn't been deleted when the created object is deleted
     QVERIFY(engine.rootContext() != nullptr);
+}
+
+void tst_DeclarativeView::instantiateQQuickWidgetWithContextProperty()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine);
+    component.setData("import Qt.Widgets 1.0;\n"
+                      "DeclarativeView { \n"
+                      "    source: \"qrc:///rectangle.qml\"\n"
+                      "    rootContext: DeclarativeContext {\n"
+                      "        DeclarativeContextProperty {\n"
+                      "            name: \"_colour\"\n"
+                      "            value: \"lightblue\"\n"
+                      "        }\n"
+                      "    }\n"
+                      "}", QUrl());
+    QScopedPointer<QObject> object(component.create());
+    QVERIFY(!object.isNull());
+
+    QQuickWidget *rootView = qobject_cast<QQuickWidget*>(object.data());
+    QVERIFY(rootView != nullptr);
+
+    QQuickItem *rootItem = rootView->rootObject();
+    QVERIFY(rootItem != nullptr);
+
+    QQmlContext *context = QQmlEngine::contextForObject(rootItem);
+    QVERIFY(context != nullptr);
+
+    QVariant contextProperty = context->contextProperty(QStringLiteral("_colour"));
+    QVERIFY(!contextProperty.isNull() && contextProperty.isValid());
 }
 
 QTEST_MAIN(tst_DeclarativeView)
